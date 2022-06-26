5c4211e57a31f37426d8ae58b078236b6f93948c
EmmaMaddocks <104844501+EmmaMaddocks@users.noreply.github.com>
2022-06-26 17:17:05 +0100

    Delete drum kit directory

--- drum kit/app.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('keydown', function(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keycode}"]`);
-    const key = document.querySelector(`key[data-key="${e.keycode}"]`);
-    if(!audio) return; //stop function running
-    audio.currentTime = 0; //rewind to start
-    audio.play();
-    key.addClass('playing');
-    key.classList.add('playing');
-    });
